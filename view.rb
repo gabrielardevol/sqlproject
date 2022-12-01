f8c62055552ff0c7985df79a022a90164ba8358a
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-01 18:48:50 +0100

    code splitted between a view file and a router file

---
+++ view.rb
@@ -0,0 +1,18 @@
+class View
+  def display_options
+    puts "1 - Display existing data"
+    puts "2 - Add data"
+    puts "3 - Export data to SVG / Json"
+
+    return gets.chomp
+  end
+
+  def add_data
+    # real life personality
+      # name
+      # tags
+    # fictional character
+    # narrative and books
+    #
+  end
+end
