9b4dfc53cf2d53505053168ac5208ca55399b3b6
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-10 21:10:12 +0100

    added work types in schema

--- seed.rb
+++ seed.rb
@@ -2,7 +2,10 @@
 require 'sqlite3'
 db = SQLite3::Database.new("db.db")
 db.execute("DROP TABLE `people`")
- db.execute("DROP TABLE `work`")
+db.execute("DROP TABLE `works`")
+db.execute("DROP TABLE `work_types`")
+
+
 
 db.execute("CREATE TABLE `people` (
   `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,13 +18,20 @@ db.execute("INSERT INTO people (name, birth, description)
   VALUES ('Gabriel', '1996-11-13', 'sóc jo')
   ")
 
-  db.execute("CREATE TABLE `work` (
+  db.execute("CREATE TABLE `works` (
     `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
     `name` TEXT,
     `birth` DATE,
     `death` DATE,
     `description` TEXT
   );")
-  db.execute("INSERT INTO work (name, birth, description)
+
+  db.execute("CREATE TABLE `work_types` (
+    `id`  INTEGER PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT,
+    `description` TEXT
+  );")
+
+  db.execute("INSERT INTO works (name, birth, description)
   VALUES ('Gabriela', '1996-11-13', 'sóc jo')
   ")
