870b6fa24d646671f984ca91e1c50e00b1f04b9b
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-11 14:36:10 +0100

    written intro in app.eb

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require_relative "router"
 
-puts "app test"
+puts "Aquesta base de dades té l'objectiu de recopilar referències de la cultura LGBT+ en el context catalanoparlant."
+puts "Escull una opció:"
 r = Router.new
