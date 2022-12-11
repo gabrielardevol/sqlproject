b8bbd91fc143452bea3bdaa0202fa48649ccd9a4
Gabriel Ardevol <artsdevol@gmail.com>
2022-12-11 08:06:29 +0100

    found out how to do a sql query from ruby

--- controller.rb
+++ controller.rb
@@ -14,12 +14,17 @@ class Controller
   end
 
   def add
-    name = @view.ask_for("name")
+    value = 'birth'
+    params = {}
+    params[:name] = @view.ask_for("name")
     date_of_birth = @view.ask_for("date of birth")
     defunction_date = @view.ask_for("defunction date")
     description = @view.ask_for("description")
-    @db.execute("INSERT INTO people (name, birth, death, description)
-                VALUES (#{name}, #{date_of_birth}, #{defunction_date}, #{description}); ")
+   # @db.execute("INSERT INTO people (name, birth, death, description)
+   #             VALUES (#{name}, #{date_of_birth}, #{defunction_date}, #{description}); ")
+    query = "INSERT INTO people (name)
+                VALUES (?)"
+@db.execute(query, params[:name])
 
   end
 
